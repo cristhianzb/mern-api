ad66a729bffe732358913f703ae5066b2fb60312
Cristhian Zenteno Brañez <cristhianzentenob@gmail.com>
2020-11-02 21:19:00 +0000

    added routes for videos

--- src/routes/videos.routes.ts
+++ src/routes/videos.routes.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
+import { getVideos, getVideo, createVideo, deleteVideo, updateVideo } from "../controllers/videos.controller";
+
 
 const router = Router();
 
-router.get('/videos', (req, res) => res.json("getting videos"));
+router.get('/videos', getVideos);
+
+router.get('/videos/:id', getVideo);
+
+router.post('/videos', createVideo);
+
+router.delete('/videos/:id', deleteVideo);
+
+router.put('/videos/:id', updateVideo);
 
 export default router
